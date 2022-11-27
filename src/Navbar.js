810ceeadf6632afb69a5a300b85fa18854b31497
Senayt Gobezie <senaitgobezie@gmail.com>
2022-11-27 22:32:47 +0300

    seting up the project

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Nany Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create"></a>
+                <a href="/"> New blog</a>
+            </div>
+        </nav>
+    
+     );
+}
+ 
+export default Navbar;
