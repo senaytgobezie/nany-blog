c07c2d10bcddac033ba8ced97b9518cf8147a162
Senayt Gobezie <senaitgobezie@gmail.com>
2022-11-30 01:16:18 +0300

    adding the blogList component

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,25 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
 const [blogs, setBlogs]=useState([
- { title: 'My new website', body: 'lorem ipsum...', author: 'mar', id: 1 },
+ { title: 'My new website', body: 'lorem ipsum...', author: 'nany', id: 1 },
 { title: 'Welcome party!', body: 'lorem ipsum...', author: 'kali', id: 2 },
 { title: 'My pink friday', body: 'lorem ipsum...', author: 'nany', id: 3 }]);
 
 
-const handleClick =() =>{
-
+const handleDelete= (id) =>{
+const newBlogs = blogs.filter(blog=> blog.id !== id);
+setBlogs(newBlogs);
     }
 
     return ( 
         <div className="home">
-            { blogs.map((blog) =>(
-                <div className="blog-preview" key={blog.id} >
-                   <h2>{blog.title}</h2>
-                   <p> Written by {blog.author}</p>
-                </div>))}
+           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> 
+           {/* <BlogList blogs={blogs.filter( (blog)=> blog.author=== 'nany' )} title="nany's Blogs" />  */}
 
+           {/* this is the props */}
         </div>
      );
 }
