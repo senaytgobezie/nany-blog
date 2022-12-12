caf7ab8b42cb877edd26c1dca8c1e1973f66b170
Senayt Gobezie <senaitgobezie@gmail.com>
2022-12-12 21:53:43 +0300

    json file created

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useState ,useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "nany", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "kali", id: 2 },
-    { title: "My pink friday", body: "lorem ipsum...", author: "nany", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
-
+ useEffect(()=> {
+  fetch("http://localhost:8000/blogs")
+  .then(res=>{ return res.json();})
+  .then(data=>{setBlogs(data); })
+ },[]);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {/* <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> */}
       {/* <BlogList blogs={blogs.filter( (blog)=> blog.author=== 'nany' )} title="nany's Blogs" />  */}
 
       {/* this is the props */}
